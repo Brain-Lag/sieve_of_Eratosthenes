993369a20b5b9f864472fbd898adb2b7e74f478e
levchenkolk <levchenko.levan@mail.ru>
2022-09-17 14:35:37 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+Stopwatch stopwatch = new();
+while (true)
+{
+    Console.WriteLine("Введите число");
+    int N = int.Parse(Console.ReadLine());
+
+    stopwatch.Start();
+
+    bool[] A = new bool[N];
+
+    double sqrt = Math.Sqrt(N) + 1;
+
+    for (int i = 2; i < sqrt; i++)
+    {
+        if (!A[i])
+        {
+            for (int j = i * i; j < N; j += i)
+            {
+                A[j] = true;
+            }
+        }
+    }
+    Console.WriteLine();
+
+    for (int i = 2; i < N; i++)
+    {
+        if (!A[i])
+        {
+            Console.Write("{0} ", i);
+        }
+    }
+    stopwatch.Stop();
+    Console.WriteLine($"--------Прошло милисекунд: {stopwatch.ElapsedMilliseconds}");
+    Console.WriteLine();
+}
